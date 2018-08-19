653f416f5ed6285b15a5ec757ba752c21ddc0d46
miguegallo88ss <miguegallo88@hotmail.com>
2018-08-19 13:46:29 -0500

    Adding options to main Window

--- Qt_C_Code_serial_communication/build-read_USB-Desktop_Qt_5_11_1_MinGW_32bit-Debug/ui_mainwindow.h
+++ Qt_C_Code_serial_communication/build-read_USB-Desktop_Qt_5_11_1_MinGW_32bit-Debug/ui_mainwindow.h
@@ -10,8 +10,10 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
@@ -22,28 +24,51 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
+    QAction *actionAbout;
+    QAction *actionBud_Rate;
     QWidget *centralWidget;
+    QMenuBar *menuBar;
+    QMenu *menuFile;
+    QMenu *menuView;
+    QMenu *menuExit;
+    QToolBar *mainToolBar;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
-        menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        MainWindow->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
+        MainWindow->resize(636, 474);
+        actionAbout = new QAction(MainWindow);
+        actionAbout->setObjectName(QStringLiteral("actionAbout"));
+        actionBud_Rate = new QAction(MainWindow);
+        actionBud_Rate->setObjectName(QStringLiteral("actionBud_Rate"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 636, 21));
+        menuFile = new QMenu(menuBar);
+        menuFile->setObjectName(QStringLiteral("menuFile"));
+        menuView = new QMenu(menuBar);
+        menuView->setObjectName(QStringLiteral("menuView"));
+        menuExit = new QMenu(menuBar);
+        menuExit->setObjectName(QStringLiteral("menuExit"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuFile->menuAction());
+        menuBar->addAction(menuView->menuAction());
+        menuBar->addAction(menuExit->menuAction());
+        menuFile->addSeparator();
+        menuFile->addAction(actionBud_Rate);
+        menuFile->addAction(actionAbout);
 
         retranslateUi(MainWindow);
 
@@ -53,6 +78,11 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionAbout->setText(QApplication::translate("MainWindow", "About", nullptr));
+        actionBud_Rate->setText(QApplication::translate("MainWindow", "Bud Rate", nullptr));
+        menuFile->setTitle(QApplication::translate("MainWindow", "File", nullptr));
+        menuView->setTitle(QApplication::translate("MainWindow", "View", nullptr));
+        menuExit->setTitle(QApplication::translate("MainWindow", "Exit", nullptr));
     } // retranslateUi
 
 };
